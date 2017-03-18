c91a8a528a4fc3b0836e210ff645425e1c2049dd
albert.buchard <antinabi87>
2017-03-18 13:32:07 +0100

    grid density

--- challenges/rover/examples/python/astar.py
+++ challenges/rover/examples/python/astar.py
@@ -7,11 +7,25 @@ V = time_to_go + (remaining_density * time_per_density)
 # Model
 
 # Density
-def getDensityGrid():
+def getDensityGrid(bins = 10):
     grid = np.zeros((10, 10))
+    cellWidth = self.max_x / bins
+    cellHeight = self.max_y / bins
+
+    for x in range(0,bins):
+        for y in range (0,bins):
+            grid[x,y] = getNumberOfPointInCell()
     return grid
 
-def getNumberOfPointInCell(point, cell):
+def pointsInCell (cell, onlyNonVisited = True):
+    points = []
+    for point in getNonVisitedGoodPoints():
+        if (isPointInCell(point, cell):
+            points.append(point)
+
+    return points
+
+def isPointInCell(point, cell):
     px = point[0]
     py = point[1]
     cellx = cell[0]
@@ -27,15 +41,17 @@ def getDensityForPoint(totalPoints):
     # or you compute the local density
     # density radius should depend on remaining time
     remainingPoints = len(self.visitedGoodPoints)
+    vicinityPoint = self.pointCloserThan(self.remainingDistance())
 
-
-    return density
+    return (len(vicinityPoint)/remainingPoints)
 
 def pointCloserThan(distance, fromPoint = None):
     if fromPoint is None:
         fromPoint = self.currentPositionPoint()
 
-
+    points = []
     for point in self.getNonVisitedGoodPoints():
         if (euclidianDistance(fromPoint, point) < distance):
-            
+            points.append(point)
+
+    return points
