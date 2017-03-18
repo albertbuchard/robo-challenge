e904c24e76241695815c019df1492bf45d700cb9
albert.buchard <antinabi87>
2017-03-18 11:50:35 +0100

    star

--- challenges/rover/examples/python/astar.py
+++ challenges/rover/examples/python/astar.py
@@ -1,6 +1,19 @@
+import numpy as np
+
 # Create a density map from point position
 
 # Value function
 V = time_to_go + (remaining_density * time_per_density)
 
-# model
+# Model
+
+# Density
+def getDensityGrid():
+    grid = []
+    return grid
+
+def getDensityForPoint():
+    # either you place the point in the grid a get the value of the grid
+    # or you compute the local density
+    # density radius should depend on remaining time
+    return density
