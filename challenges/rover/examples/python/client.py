1b1a9f009eaacb5b841abc016b5c7886a5601897
Anand Singh <anand9522s@gmail.com>
2017-03-18 15:55:23 +0530

    nearest

--- challenges/rover/examples/python/client.py
+++ challenges/rover/examples/python/client.py
@@ -11,14 +11,9 @@ PLAYER_NAME = "TheRegressor"
 GAME_STATE = 0 # 0 is waiting, 1 is playing
 DISTANCE_BETWEEN_WEELS = 4.8*180/m.pi
 CONVERT_COUNT_DIST = 3/10
-
 game_data = {}
-goodPoints = numpy.zeros(shape=(45,2))
-badPoints = numpy.zeros(shape=(5,2))
-targetPoint = 0 #index of goodPoints which is the current target
 game_log = []
 i = 0
-
 
 class Robot(object):
     """docstring for ClassName"""
@@ -28,19 +23,27 @@ class Robot(object):
         self.rightCount = 0
         self.leftCount = 0
         self.angle = 0
-
+        self.currentTarget = {}
+        self.goodPoints = []
+        self.badPoints = []
+        self.visitedGoodPoints = []
+        self.visitedBadPoints = []
+    
+    
+    
+    
     def moveForward(self, value):
         client.publish('robot/process', '{"command": "forward", "args": ' + str(value) + '}', qos=0, retain=False)
-
+    
     def moveBackward(self, value):
         client.publish('robot/process', '{"command": "backward", "args": ' + str(value) + '}', qos=0, retain=False)
-
+    
     def turnRight(self, degree):
         client.publish('robot/process', '{"command": "right", "args": ' + str(degree) + '}', qos=0, retain=False)
-
+    
     def turnLeft(self, degree):
         client.publish('robot/process', '{"command": "left", "args": ' + str(degree) + '}', qos=0, retain=False)
-
+    
     def increment(self, vRight, vLeft):
         dRight = vRight - self.rightCount
         dLeft = vLeft - self.leftCount
@@ -51,7 +54,7 @@ class Robot(object):
         self.angle = self.angle + dAngle*180/m.pi
         self.rightCount = vRight
         self.leftCount = vLeft
-
+    
     def getTargetAngle(self, xTarget, yTarget):
         xDiff = self.x - xTarget
         yDiff = self.x - yTarget
@@ -60,18 +63,44 @@ class Robot(object):
             if xDiff < 0:
                 targetAngle = 360 - targetAngle
         else:
-             if xDiff > 0:
-                 targetAngle = 180 - targetAngle
-             else:
-                 targetAngle = 180 + targetAngle
+            if xDiff > 0:
+                targetAngle = 180 - targetAngle
+                    else:
+                        targetAngle = 180 + targetAngle
         return targetAngle
+            
+                def getNonVisitedGoodPoints():
+        # loop through positive points
+        points = []
+        for (index, point) in enumerate(self.goodPoints):
+            if index in self.visitedGoodPoints:
+                points.append(point)
+            pass
+                
+    return points
+
+def getNearestNeighbour(fromPoint = [self.x, self.y]):
+    self.currentTarget
+        minDistance = 100000
+        nextSucker = None
+        for point in self.getNonVisitedGoodPoints()):
+            
+            distance= np.sqrt(np.dot((point-fromPoint),(point-fromPoint)))
+                if minDistance < distance:
+                    nextSucker=point
+    
+
+    return nextSucker;
+
+
+
 
 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-
+    
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe('players/' + PLAYER_NAME + '/#')
@@ -88,14 +117,14 @@ def on_message(client, userdata, msg):
     global GAME_STATE
     global game_log
     global i
-
+    
     #print(msg.topic)
     obj = json.loads(msg.payload.decode("utf-8"))
     #print(obj)
-
+    
     game_log.append(obj)
-
-
+    
+    
     if GAME_STATE == 1:
         if msg.topic == 'robot/state':
             robot.increment(obj['right_motor'], obj['left_motor'])
@@ -105,7 +134,7 @@ def on_message(client, userdata, msg):
             #print(robot.angle)
             if robot.angle > 200:
                 exit()
-
+    
         elif (msg.topic == 'players/%s/game' % PLAYER_NAME):
             print("********** STORED GAME_DATA ************")
             game_data = obj
@@ -115,11 +144,11 @@ def on_message(client, userdata, msg):
             robot.moveForward(10)
 
 
-    elif (GAME_STATE == 0):
-        if ((msg.topic=='players/%s/incoming' % PLAYER_NAME) and ("command" in obj)):
-                if (obj['command'] == "start"):
-                    print("********** RECEIVED START FROM SERVER ************")
-                    client.publish('players/' + PLAYER_NAME , '{"command": "start"}')
+elif (GAME_STATE == 0):
+    if ((msg.topic=='players/%s/incoming' % PLAYER_NAME) and ("command" in obj)):
+        if (obj['command'] == "start"):
+            print("********** RECEIVED START FROM SERVER ************")
+                client.publish('players/' + PLAYER_NAME , '{"command": "start"}')
                     GAME_STATE = 1
                 elif (obj['command'] == "finished"):
                     print("********** RECEIVED FINISHED FROM SERVER ************")
@@ -128,7 +157,7 @@ def on_message(client, userdata, msg):
                     exit()
 
 
-    i += 1
+i += 1
     if (i>=10):
         i = 0
         with open("data.txt","w") as f: #in write mode
@@ -136,29 +165,22 @@ def on_message(client, userdata, msg):
 
 
 
-def generateTargets(points):
-    goodCounter = 0
-    badCounter = 0
-    for point in points:
-        if point["score"] == 1:
-            goodPoints[goodCounter] = [point["x"], point["y"]]
-            goodCounter++
-        else:
-            badPoints[badCounter] = [point["x"], point["y"]]
-            badCounter++
-    print("Good points:" + goodPoints)
-    print("Bad points:" + badPoints)
+
+
+
+
+
 
 
 if __name__ == '__main__':
-
+    
     robot = Robot()
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-
+    
     client.connect(SERVER, PORT, 60)
-
+    
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
     # Other loop*() functions are available that give a threaded interface and a
